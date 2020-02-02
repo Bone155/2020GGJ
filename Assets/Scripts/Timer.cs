27949dec197fcd07a74dec69cc38c92fd54a0f01
Bone155 <s199010@students.aie.edu>
2020-02-01 18:27:08 -0800

    first commit

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Timer : MonoBehaviour
+{
+    public float timer;
+    public Text text;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        text.text = "" + (int)timer;
+        timer -= Time.deltaTime;
+        if(timer <= 0)
+        {
+            SceneManager.LoadScene("NightManagerv1Grey");
+        }
+    }
+}
